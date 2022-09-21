326aad0f03b7effe665bb6ef25cc56d0bb04ce03
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2022-09-20 21:04:28 -0300

    feat: add binary search algorithm

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0
+    let hi = haystack.length
 
+    do {
+        const m = Math.floor(lo + (hi - lo) / 2)
+        const v = haystack[m]
+
+        if (needle === v) {
+            return true
+        } else if (needle > v) {
+            lo = m + 1
+        } else {
+            hi = m
+        }
+    } while (lo < hi)
+    
+    return false
 }
--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,4 @@
+// O(N)
 export default function linear_search(haystack: number[], needle: number): boolean {
     for (let i = 0; i < haystack.length; i++) {
         if (haystack[i] === needle) {
