8b0ead8f645797adeee5e32ecfb528737b11d106
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2022-09-21 22:03:11 -0300

    feat: solve problem crystal ball

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,4 @@
+// O log(N)
 export default function bs_list(haystack: number[], needle: number): boolean {
     let lo = 0
     let hi = haystack.length
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
+// O sqrt(N) + sqrt(N)
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jmpAmount = Math.floor(Math.sqrt(breaks.length))
 
+    let i = jmpAmount
+
+    for (; i < breaks.length; i += jmpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jmpAmount
+
+    for (let j = 0; j < jmpAmount && j < breaks.length; ++j, ++i) {
+        if (breaks[i]) {
+            return i
+        }
+    }
+    return -1
 }
