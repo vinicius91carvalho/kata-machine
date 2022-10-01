6c5e7817a22c05e5f05e0d893167a6039c4644cc
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2022-10-01 18:56:49 -0300

    feat: add stack algorithm

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,45 @@
+export class Node<T> {
+    constructor(public readonly item: T, public prev?: Node<T>) { }
+}
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>
 
     constructor() {
+        this.length = 0
     }
 
     push(item: T): void {
+        const node = new Node(item)
+        this.length++
+        
+        if (!this.head) {
+            this.head = node
+            return
+        }
 
-}
+        const head = this.head
+        this.head = node
+        this.head.prev = head
+    }
+
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined
+        }
 
-}
+        const head = this.head
+        this.head = this.head.prev
+
+        head.prev = undefined
+
+        this.length--
+
+        return head.item
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.item
+    }
 }
